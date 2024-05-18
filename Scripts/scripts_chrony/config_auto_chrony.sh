a2ec2735c71b25ec023447ba485496adebf13ccc
shadowthib <thibault.herry@std.heh.be>
2024-05-18 15:16:44 +0200

    autoconfig chrony

---
+++ Scripts/scripts_chrony/config_auto_chrony.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Fichier de configuration créé dans le but d'automatiser
+# l'installation et la configuration du service chrony.
+
+echo "Début de l'installation et de la configuration de chrony"
+echo "--------------------------------------------------------"
+
+source ../config.cfg
+
+#démarrage du service chrony
+systemctl start chronyd
+systemctl enable chronyd
+
+cd /etc/ 
+
+#configuration du service chrony
+echo " allow $ADDRESS/24" >> /etc/chrony.conf
+echo "server $IPADD iburst" >> /etc/chrony.conf
+
+#redémarrage du service chrony
+systemctl restart chronyd
+
+echo "Fin de l'installation et de la configuration de chrony"
+echo "-------------------------------------------------------"
+echo ""
+echo "Test de l'installation et de la configuration de chrony"
+echo "-------------------------------------------------------"
+echo ""
+
+#test de la configuration du service chrony
+chronyc sources -c
+chronyc tracking
