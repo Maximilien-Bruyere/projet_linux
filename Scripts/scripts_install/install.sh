85c3ead89d114fac6f6a9737e456aff71dde2d44
Maximilien-Bruyere <58529462+Maximilien-Bruyere@users.noreply.github.com>
2024-05-19 03:58:16 +0200

    Update install.sh

--- Scripts/scripts_install/install.sh
+++ Scripts/scripts_install/install.sh
@@ -2,15 +2,15 @@
 
 # Installation des paquets nécessaires 
 sudo dnf -y install nfs-utils
-#sudo dnf -y install httpd
-#sudo dnf -y install samba
+sudo dnf -y install httpd
+sudo dnf -y install samba
 sudo dnf -y install bind
 sudo dnf -y install chrony
-#sudo dnf -y install fail2ban
-#sudo dnf -y install vsftpd
-#sudo dnf -y install clamav
-#sudo dnf -y install lynx
-#sudo dnf -y --nogpgcheck install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-#sudo dnf -y --nogpgcheck install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
-#sudo dnf -y --nogpgcheck install https://rpms.remirepo.net/enterprise/9/remi/x86_64/php-fedora-autoloader-1.0.1-2.el9.remi.noarch.rpm
-#sudo dnf -y --enablerepo=remi install phpMyAdmin
+sudo dnf -y install fail2ban
+sudo dnf -y install vsftpd
+sudo dnf -y install clamav
+sudo dnf -y install lynx
+sudo dnf -y --nogpgcheck install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+sudo dnf -y --nogpgcheck install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
+sudo dnf -y --nogpgcheck install https://rpms.remirepo.net/enterprise/9/remi/x86_64/php-fedora-autoloader-1.0.1-2.el9.remi.noarch.rpm
+sudo dnf -y --enablerepo=remi install phpMyAdmin
