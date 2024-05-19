84ac86395e3a69c08a6183366888035627405855
shadowthib <thibault.herry@std.heh.be>
2024-05-19 23:16:17 +0200

    Create clamav.sh

---
+++ Scripts/scripts_clamav/clamav.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Fichier de configuration créé dans le but d'automatiser la mise en place de ClamAV
+# pour sécuriser un serveur.
+
+echo "Configuration de ClamAV"
+echo "-------------------------"
+echo ""
+
+# Installation de ClamAV
+
+sudo dnf install clamav clamd -y
+dnf --enablerepo=epel -y install clamav clamav-update
+
+# Configuration de ClamAV
+
+# nano /etc/freshclam.conf # si tu veux look le fichier de conf
+#   # si tu veux look le fichier de conf
+
+mkdir /var/log/clamav
+touch /var/log/clamav/clamav-scan.log
+
+echo "0 12 3 * * freshclam" >> /etc/crontab
+echo "30 12 * * * /usr/bin/clamscan -ri / >> /var/log/clamav/clamav-scan.log" >> /etc/crontab
+
+echo "Configuration de ClamAV terminée."
+echo "-------------------------"
+echo ""
