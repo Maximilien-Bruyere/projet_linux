34b396e4fb15107bcc6f0559ef4b19c5f58931b4
shadowthib <thibault.herry@std.heh.be>
2024-05-21 15:12:31 +0200

    Update rsync.sh

--- Scripts/scripts_backup/rsync.sh
+++ Scripts/scripts_backup/rsync.sh
@@ -14,34 +14,45 @@ dnf -y install rsync
 
 # Création du script de sauvegarde
 
-#mkdir /backup
-#touch /backup/backup.sh
-touch /etc/backup.sh
-echo "#!/bin/bash" > /etc/backup.sh
-echo "" >> /etc/backup.sh
-echo "# Script de sauvegarde" >> /etc/backup.sh
-echo "" >> /etc/backup.sh
-# echo "rsync -av --delete /web /backup/" >> /backup/backup.sh
-# rsync -avz --delete /srv/ admin@192.168.1.170:/backup/srv >> /etc/backup.sh
-# rsync -avz --delete /var/ admin@192.168.1.170:/backup/var >> /etc/backup.sh
-# rsync -avz --delete /etc/ admin@192.168.1.170:/backup/etc >> /etc/backup.sh
-rsync -avz --delete /etc/ /backup/etc/"$(date +%Y-%m-%d)_etc" >> /etc/backup.sh
-rsync -avz --delete /var/ /backup/var/"$(date +%Y-%m-%d)_var" >> /etc/backup.sh
-rsync -avz --delete /srv/ /backup/etc/"$(date +%Y-%m-%d)_srv" >> /etc/backup.sh
-rsync -avz --delete /home/ /backup/home/"$(date +%Y-%m-%d)_home >> /etc/backup.sh
+
+#touch /etc/backup.sh
+#echo "#!/bin/bash" > /etc/backup.sh
+#echo "" >> /etc/backup.sh
+#echo "# Script de sauvegarde" >> /etc/backup.sh
+#echo "" >> /etc/backup.sh
+touch /sbin/test.sh
+echo '#!/bin/bash' > /sbin/test.sh
+echo '' >> /sbin/test.sh
+echo '# Script de sauvegarde' >> /sbin/test.sh
+echo '' >> /sbin/test.sh
+#mkdir /backup/etc
+#mkdir /backup/var
+#mkfir /backup/srv
+#mkdir /backup/home
+mkdir /home/admin/backup
+mkdir /home/admin/backup/etc
+mkdir /home/admin/backup/var
+mkdir /home/admin/backup/srv
 
 
+#echo 'rsync -avz --delete /etc/ /backup/etc/"$(date +%Y-%m-%d)_etc"' >> /etc/backup.sh
+#echo 'rsync -avz --delete /var/ /backup/var/"$(date +%Y-%m-%d)_var"' >> /etc/backup.sh
+#echo 'rsync -avz --delete /srv/ /backup/etc/"$(date +%Y-%m-%d)_srv"' >> /etc/backup.sh
+#echo 'rsync -avz --delete /home/ /backup/home/"$(date +%Y-%m-%d)_home"' >> /etc/backup.sh
+echo 'rsync -avz --delete /etc/ /home/admin/backup/etc/"$(date +%Y-%m-%d)_etc"' >> /sbin/test.sh
+echo 'rsync -avz --delete /var/ /home/admin/backup/var/"$(date +%Y-%m-%d)_var"' >> /sbin/test.sh
+echo 'rsync -avz --delete /srv/ /home/admin/backup/etc/"$(date +%Y-%m-%d)_srv"' >> /sbin/test.sh
 
-#rsync -avz --delete /home admin@192.168.1.170:/backup/ >> /etc/backup.sh
-
-chmod +x /backup/backup.sh
-
+#chmod +x /backup/backup.sh
+chmod +x /sbin/test.sh
 
 # Création de la tâche cron
 
-# echo "0 13 * * * /backup/backup.sh" >> /etc/crontab
-bash -c "(crontab -l 2>/dev/null; echo '0 12 * * * /backup/backup.sh') | crontab -"
-
+#bash -c "(crontab -l 2>/dev/null; echo '0 12 * * * /backup/backup.sh') | crontab -"
+bash -c "(crontab -l 2>/dev/null; echo '0 12 * * * /sbin/test.sh') | crontab -"
 echo "Configuration de rsync terminée."
 echo "-------------------------"
 echo ""
+
+
+
