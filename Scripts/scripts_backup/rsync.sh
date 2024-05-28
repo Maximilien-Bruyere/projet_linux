9318d34523b4ace0adc50fca27b1461dd5e02d1d
Bruyère Maximilien <58529462+Maximilien-Bruyere@users.noreply.github.com>
2024-05-28 02:24:09 +0200

    Update rsync.sh

--- Scripts/scripts_backup/rsync.sh
+++ Scripts/scripts_backup/rsync.sh
@@ -16,7 +16,7 @@ mkdir /backup/home
 
 echo 'rsync -avz --delete /etc/ /backup/etc/"$(date +%Y-%m-%d)_etc"' >> /sbin/dailybackup.sh
 echo 'rsync -avz --delete /var/ /backup/var/"$(date +%Y-%m-%d)_var"' >> /sbin/dailybackup.sh
-echo 'rsync -avz --delete /srv/ /backup/etc/"$(date +%Y-%m-%d)_srv"' >> /sbin/dailybackup.sh
+echo 'rsync -avz --delete /srv/ /backup/srv/"$(date +%Y-%m-%d)_srv"' >> /sbin/dailybackup.sh
 echo 'rsync -avz --delete /home/ /backup/home/"$(date +%Y-%m-%d)_home"' >> /sbin/dailybackup.sh
 chmod +x /sbin/dailybackup.sh
 
